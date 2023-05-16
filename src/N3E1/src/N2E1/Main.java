93a39c3777b9afe97a52f0d6da0acd78c578fd29
Parapau2 <120369026+Parapau2@users.noreply.github.com>
2023-05-16 09:28:05 +0200

    Add files via upload

---
+++ src/N3E1/src/N2E1/Main.java
@@ -0,0 +1,16 @@
+package N2E1;
+
+public class Main {
+	public static void main (String args[]) {
+		Entrada.llegirByte("Bite vol dir mosega en angles");
+		Entrada.llegirChar("Char");
+		Entrada.llegirDouble("Double");
+		Entrada.llegirFloat("Flotador");
+		Entrada.llegirInt("Intencionadament");
+		Entrada.llegirString("Fil");
+		Entrada.llegirSiNo("si o no");
+		
+		
+		System.out.println("Chimpon");
+	}
+}
